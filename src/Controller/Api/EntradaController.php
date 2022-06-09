fd8a2f231b2922fee4a1ac4e36d2b12c84201611
Yulml <juliomoreno@gmail.com>
2022-06-09 16:55:00 +0200

    Refresh token

--- src/Controller/Api/EntradaController.php
+++ src/Controller/Api/EntradaController.php
@@ -2,19 +2,34 @@
 
 namespace App\Controller\Api;
 
+use App\Repository\EntradaRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Component\Pager\PaginatorInterface;
 
 class EntradaController extends AbstractController
 {
     /**
-     * @Route("/api/entrada", name="app_api_entrada")
+     * @Route("/api/entrada", methods={"GET"})
      */
-    public function index(): Response
+    public function index(Request $request, EntradaRepository $entradaRepository, PaginatorInterface $paginator): Response
     {
-        return $this->render('api/entrada/index.html.twig', [
-            'controller_name' => 'EntradaController',
-        ]);
+        $currentEntrada = $request->query->get('page', 1);
+        $query = $entradaRepository->getQueryAll();
+
+        $entradas = $paginator->paginate($query, $currentEntrada, 10);
+        $resultado = [];
+
+        foreach ($entradas as $entrada) {
+            $resultado[] = [
+                'id' => $entrada->getId(),
+                'fecha' => $entrada->getFecha()->format('Y-m-d H:i:s'),
+                'slug' => $entrada->getSlug(),
+                'titulo' => $entrada->getTitulo(),
+            ];
+        }
+        return $this->json($resultado);
     }
 }
