27061727b12ecf1958a90aade87cf8c81865460e
Yulml <juliomoreno@gmail.com>
2022-06-09 18:38:57 +0200

    Cambios filtros

--- src/Controller/Api/EntradaController.php
+++ src/Controller/Api/EntradaController.php
@@ -16,10 +16,15 @@ class EntradaController extends AbstractController
      */
     public function index(Request $request, EntradaRepository $entradaRepository, PaginatorInterface $paginator): Response
     {
-        $currentEntrada = $request->query->get('page', 1);
-        $query = $entradaRepository->getQueryAll();
+        // $fecha = new \DateTime('2022-06-09 00:00:00'); particularidad de php, al hacer una fecha si no indicamos
+        //la hora te pone automaticamente la hora actual
 
-        $entradas = $paginator->paginate($query, $currentEntrada, 10);
+        $currentPage = $request->query->get('page', 1);
+        $filter = $request->query->all();
+        //$query = $entradaRepository->getQueryAll();
+        $query = $entradaRepository->getQueryByFilter($filter);
+
+        $entradas = $paginator->paginate($query, $currentPage, 10);
         $resultado = [];
 
         foreach ($entradas as $entrada) {
@@ -28,6 +33,9 @@ class EntradaController extends AbstractController
                 'fecha' => $entrada->getFecha()->format('Y-m-d H:i:s'),
                 'slug' => $entrada->getSlug(),
                 'titulo' => $entrada->getTitulo(),
+                'usuario' => $entrada->getUsuario()->getEmail(),
+                'categoria' => $entrada->getCategoria()->getId(),
+                'espacio' => $entrada->getCategoria()->getEspacio()->getId()
             ];
         }
         return $this->json($resultado);
